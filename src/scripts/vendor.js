1888d17c86ca65123949214f69c9532de26b7198
Ewan Burns <ewan@beezup.com>
2018-11-23 17:09:56 +0100

    From typescript

--- src/scripts/vendor.js
+++
@@ -1,6 +0,0 @@
-//  EBU: Importing styles, not a fan of this
-import "../styles/vendor.scss"
-
-import $ from "jquery";
-import popper from "popper.js";
-import bootstrap from "bootstrap";
---
+++ src/scripts/vendor.ts
@@ -0,0 +1,6 @@
+//  EBU: Importing styles, not a fan of this
+import "../styles/vendor.scss"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
