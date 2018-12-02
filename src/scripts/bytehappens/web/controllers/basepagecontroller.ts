613977e2cb228036d36f91c2124c1c8e62b2d4d9
Ewan Burns <ewan@beezup.com>
2018-12-02 20:29:35 +0100

    Footer theme

--- src/scripts/bytehappens/web/controllers/basepagecontroller.ts
+++ src/scripts/bytehappens/web/controllers/basepagecontroller.ts
@@ -1,6 +1,5 @@
 export abstract class BasePageController {
   constructor($rootScope: IByteHappensRootscopeService, pageTitle: string) {
     $rootScope.pageTitle = pageTitle;
-    console.log($rootScope.pageTitle);
   }
 }
